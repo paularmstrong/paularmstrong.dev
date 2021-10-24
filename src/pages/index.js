a99e838fd2cf8b845fababb89ec75ca399d9b4f0
Paul Armstrong <paul@spaceyak.com>
2021-10-24 09:19:01 -0700

    feat: remove twitch iframe

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import classnames from 'classnames';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import classnames from "classnames";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
 function Home() {
   const context = useDocusaurusContext();
@@ -14,38 +14,26 @@ function Home() {
       title=""
       description="Paul Armstrong is a senior web application developer, specializing in JavaScript, performance, and developer experience"
     >
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
+      <header className={classnames("hero hero--primary", styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
             <Link
-              className={classnames('button button--outline button--secondary button--lg', styles.button)}
-              to={useBaseUrl('blog')}
+              className={classnames("button button--outline button--secondary button--lg", styles.button)}
+              to={useBaseUrl("blog")}
             >
               Read the Blog
             </Link>
             <Link
-              className={classnames('button button--outline button--secondary button--lg', styles.button)}
-              to={useBaseUrl('pages/protips/index')}
+              className={classnames("button button--outline button--secondary button--lg", styles.button)}
+              to={useBaseUrl("pages/protips/index")}
             >
               Learn some ProTips
             </Link>
           </div>
         </div>
       </header>
-      <main>
-        <div className="iframe">
-          <iframe
-            src="https://player.twitch.tv/?channel=paularmstrongdev&parent=localhost&parent=paularmstrong.dev"
-            height="600"
-            width="1066"
-            frameBorder="0"
-            scrolling="no"
-            allowFullScreen="allowfullscreen"
-          ></iframe>
-        </div>
-      </main>
     </Layout>
   );
 }
