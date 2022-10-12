f9a939d92ef0c6f68f495180f64433a7e09ddcb1
Paul Armstrong <paul@spaceyak.com>
2022-10-11 21:00:17 -0700

    infra: only lint changed files on commit

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	'*.{mjs,md,mdx,astro,cjs,js,ts,tsx}': 'yarn lint',
+	'*.{mjs,md,mdx,astro,cjs,js,ts,tsx}': 'yarn lint:cmd --fix',
 	'*.{tsx,ts}': () => 'yarn tsc',
 	'*': 'yarn format',
 };
