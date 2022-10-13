8618e697d841003d350f778feec1ef73532e0e4e
Paul Armstrong <paul@spaceyak.com>
2022-10-13 07:02:55 -0700

    fix: rss feed was borked

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,34 @@
 import rss from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
+const postResult = import.meta.glob('../../blog/**/*.{md,mdx}', { eager: true });
+const posts = Object.values(postResult)
+	.map((post) => {
+		if (!post.url) {
+			return false;
+		}
+		const urlMatch = post.url
+			.replace(/\.mdx?$/, '')
+			.match(/^blog\/(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})-(?<slug>.+)/);
+		if (!urlMatch) {
+			return false;
+		}
+		return {
+			link: `/blog/${Object.values(urlMatch.groups).join('/')}`,
+			title: post.frontmatter.title,
+			description: post.frontmatter.description,
+			pubDate: new Date(post.frontmatter.pubDate),
+		};
+	})
+	.filter(Boolean);
+
+console.log(posts);
+
 export const get = () =>
 	rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.{md,mdx}'),
+		items: posts,
+		stylesheet: '/rss/styles.xsl',
 	});
