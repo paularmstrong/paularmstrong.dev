2b81df3ba9cfdcfb51bfa0b56bee3c6076e0a493
Paul Armstrong <paul@spaceyak.com>
2023-05-06 08:28:54 -0700

    chore(deps): update eslint

--- lint-staged.config.cjs
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-	'*.{mjs,md,mdx,astro,cjs,js,ts,tsx}': 'yarn lint:cmd --fix',
-	'*.{tsx,ts}': () => 'yarn tsc',
-	'*.astro': () => 'yarn astro check',
-	'*': 'yarn format',
-};
