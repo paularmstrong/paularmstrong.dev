597d7f0ff49a5d3626dd7015fee7c657db6cc4ba
Paul Armstrong <paularmstrong@users.noreply.github.com>
2023-05-06 19:14:54 -0700

    refactor: use astro content collections for blog (#20)
    * refactor: use astro content collections for blog
    
    * refactor: use astro content collections for blog
    
    * fix: feeds
    
    * fix: missing posts
    
    * fix: ignore astro dir in lint and format

--- onerepo.config.js
+++ onerepo.config.js
@@ -1,7 +1,12 @@
 /** @type import('onerepo').graph.TaskConfig */
 export default {
 	'pre-commit': {
-		sequential: [{ match: '**/*.{ts,tsx,js,jsx,astro}', cmd: '$0 lint --add' }, '$0 format --add', '$0 tsc'],
+		sequential: [
+			{ match: '**/*.{ts,tsx,js,jsx,astro}', cmd: '$0 lint --add' },
+			{ cmd: '$0 astro -- sync', match: 'src/content/**' },
+			'$0 format --add',
+			'$0 tsc',
+		],
 	},
 	'pre-merge': {
 		sequential: ['$0 lint --all --no-fix', '$0 format --check', '$0 tsc', '$0 tasks -c build'],
