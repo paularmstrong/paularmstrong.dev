41f0e42d5cb092e3f3efae4a2e4524d026b9169c
Paul Armstrong <parmstrong@figure.com>
2023-08-07 08:21:51 -0700

    ci: fix onerepo config sequential -> serial

--- onerepo.config.js
+++ onerepo.config.js
@@ -1,7 +1,7 @@
 /** @type import('onerepo').graph.TaskConfig */
 export default {
 	'pre-commit': {
-		sequential: [
+		serial: [
 			{ match: '**/*.{ts,tsx,js,jsx,astro}', cmd: '$0 lint --add' },
 			{ cmd: '$0 astro -- sync', match: 'src/content/**' },
 			'$0 format --add',
@@ -9,12 +9,12 @@ export default {
 		],
 	},
 	'pre-merge': {
-		sequential: ['$0 lint --all --no-fix', '$0 format --check', '$0 tsc', '$0 tasks -c build'],
+		serial: ['$0 lint --all --no-fix', '$0 format --check', '$0 tsc', '$0 tasks -c build'],
 	},
 	'post-checkout': {
-		sequential: ['yarn'],
+		serial: ['yarn'],
 	},
 	build: {
-		sequential: ['$0 astro -- build'],
+		serial: ['$0 astro -- build'],
 	},
 };
