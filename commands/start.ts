d4738ce37a5756ef0bc96c1bc16e56a7a22ebf41
Paul Armstrong <paul@spaceyak.com>
2023-09-19 19:32:24 -0700

    fix: astro check

--- commands/start.ts
+++ commands/start.ts
@@ -1,4 +1,3 @@
-import { run } from 'onerepo';
 import type { Builder, Handler } from 'onerepo';
 
 export const command = ['start', 'dev'];
@@ -7,19 +6,12 @@ export const description = 'Run the Netlify dev server';
 
 export const builder: Builder = (yargs) => yargs.usage(`$0 ${command[0]}`);
 
-export const handler: Handler = async (argv) => {
+export const handler: Handler = async (argv, { graph }) => {
 	const { '--': rest = [] } = argv;
 
-	const [bin] = await run({
-		name: 'Get netlify bin',
-		cmd: 'yarn',
-		args: ['bin', 'ntl'],
-		runDry: true,
-	});
-
-	await run({
+	await graph.packageManager.run({
 		name: 'Run the server',
-		cmd: bin,
+		cmd: 'ntl',
 		args: ['dev', '--filter', 'paularmstrong-dev', ...rest],
 		opts: {
 			stdio: 'inherit',
