4d4e8f442bca6444504949778899c0bf2b4eeeba
Paul Armstrong <me@paularmstrong.dev>
2024-02-18 18:50:44 -0800

    blog: oneRepo announcement

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -374,6 +374,13 @@ declare module 'astro:content' {
   collection: "blog";
   data: InferEntrySchema<"blog">
 } & { render(): Render[".mdx"] };
+"2024-02-20-introducing-onerepo-the-javascript-typescript-monorepo-toolchain-for-safe-strict-fast-development.mdx": {
+	id: "2024-02-20-introducing-onerepo-the-javascript-typescript-monorepo-toolchain-for-safe-strict-fast-development.mdx";
+  slug: "2024-02-20-introducing-onerepo-the-javascript-typescript-monorepo-toolchain-for-safe-strict-fast-development";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".mdx"] };
 "2024-02-23-rust-based-javascript-linters-have-a-major-issue.mdx": {
 	id: "2024-02-23-rust-based-javascript-linters-have-a-major-issue.mdx";
   slug: "2024-02-23-rust-based-javascript-linters-have-a-major-issue";
